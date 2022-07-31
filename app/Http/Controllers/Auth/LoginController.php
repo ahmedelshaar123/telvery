c71799a89808f14fa155151bce14bf60e78bf8b9
ahmedelshaar123 <elshaar85.com>
2022-07-31 13:42:28 +0200

    elshaar

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -42,5 +43,12 @@ class LoginController extends Controller
     {
         auth()->logout();
         return redirect(url('login'));
+    }
+
+    public function credentials(Request $request)
+    {
+
+        return array_merge($request->only($this->username(), 'password'), ['is_active' => 1]);
+
     }
 }
